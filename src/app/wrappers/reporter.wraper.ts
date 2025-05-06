f4b04bc7d1c968fe56c1a7dc18fbf4c359ca737a
Misheru056 <michellemasias00@gmail.com>
2025-05-06 12:41:47 +0200

    add wrapper

---
+++ src/app/wrappers/reporter.wraper.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+import { FieldWrapper } from "@ngx-formly/core";
+
+@Component({
+    selector: 'formly-wrapper-panel',
+    template: `
+      <div class="reporter">
+       
+      </div>
+    `,
+  })
+  export class ReporterWrapper extends FieldWrapper {
+  }  
