16cfb29c68c4843259f918f9278f6d08845e6eec
jortizfdez <jortizfdez@gmail.com>
2025-05-06 12:39:01 +0200

    appmodule new validations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { AppComponent } from './app.component'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
-import { FormlyModule } from '@ngx-formly/core'
+import { FormlyFieldConfig, FormlyModule } from '@ngx-formly/core'
 import { FormlyBootstrapModule } from '@ngx-formly/bootstrap'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { FormlyFieldStepper } from './types/stepper.type'
@@ -16,6 +16,15 @@ import { FormlyFieldTextArea } from '@ngx-formly/bootstrap/textarea'
 import { FormlyFieldInput } from '@ngx-formly/bootstrap/input'
 import { FormlyFieldSelect } from '@ngx-formly/bootstrap/select'
 import { FormlyFieldRadio } from '@ngx-formly/bootstrap/radio'
+import { AbstractControl, ValidationErrors} from '@angular/forms'
+
+export function isEmployeeValidationMessage(error: any, field: FormlyFieldConfig) {
+  return `"${field?.formControl?.value}" is not a valid employee number`;
+}
+
+export function isEmployeeValidator(control: AbstractControl): ValidationErrors | null {
+  return ((control.value).toString().startsWith('N') ||  (control.value).toString().startsWith('X')) && (control.value).toString().length > 3 ? { 'employee': true } : null;
+}
 
 @NgModule({
   declarations: [AppComponent, FormlyFieldStepper],
@@ -44,8 +53,12 @@ import { FormlyFieldRadio } from '@ngx-formly/bootstrap/radio'
         { name: 'select', component: FormlyFieldSelect, wrappers: [] },
         { name: 'radiobutton', component: FormlyFieldRadio, wrappers: [] },
       ],
-    
-
+      validationMessages: [
+				{ name: 'required', message: 'This field is required' },
+        { name: 'minLength', message: 'Se requiere al menos 50 caracteres' },
+				{ name: 'employeeNumber', message: isEmployeeValidationMessage},
+			],
+			validators: [{ name: 'isEmployee', validation: isEmployeeValidator }],
     }),
   ],
   providers: [],
